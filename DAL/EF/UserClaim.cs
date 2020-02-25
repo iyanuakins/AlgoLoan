04aa5dd8ff1f20177de95faf8b4f4b289923667c
Iyanu Akins <sundax4u2@gmail.com>
2020-02-25 09:44:49 +0100

    Added and updated packages

--- DAL/EF/UserClaim.cs
+++ DAL/EF/UserClaim.cs
@@ -1,10 +1,6 @@
 namespace DAL.EF
 {
-    using System;
-    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
     public partial class UserClaim
     {
