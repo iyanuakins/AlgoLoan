6b32ccfd6b62ca624fb9254025b411c1f520a864
Iyanu Akins <sundax4u2@gmail.com>
2020-02-25 22:00:48 +0100

    Implemented search repository

---
+++ DAL/Repo/ISearchRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.EF;
+
+namespace DAL.Repo
+{
+    public interface ISearchRepository
+    {
+        void Add(Search obj);
+        List<Search> GetAll();
+
+        bool Save();
+    }
+}
---
+++ DAL/Repo/SearchRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DAL.EF;
+
+namespace DAL.Repo
+{
+    public class SearchRepository : ISearchRepository
+    {
+        private readonly AlgoLoanDbContext _context;
+
+        public SearchRepository(AlgoLoanDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Search obj)
+        {
+            try
+            {
+                _context.Searches.Add(obj);
+            }
+            catch (Exception e)
+            {
+                
+            }
+        }
+
+        public List<Search> GetAll()
+        {
+            try
+            {
+                var result = _context.Searches;
+                return result.ToList();
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        public bool Save()
+        {
+            try
+            {
+                int affectedRow = _context.SaveChanges();
+                return affectedRow > 0;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        
+    }
+}
