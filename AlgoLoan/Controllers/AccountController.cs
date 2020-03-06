e0e15eab8e44786ce323bbef4d6a001ad331dead
Iyanu Akins <sundax4u2@gmail.com>
2020-03-06 13:06:38 +0100

    Updated admin dashboard views

--- AlgoLoan/Controllers/AccountController.cs
+++ AlgoLoan/Controllers/AccountController.cs
@@ -161,7 +161,6 @@ namespace AlgoLoan.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
